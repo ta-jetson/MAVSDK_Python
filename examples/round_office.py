125087bbd7a2d856b1dc0aedf55ceacd097caa4c
ta-jetson <150237120+ta-jetson@users.noreply.github.com>
2024-11-06 12:16:43 +0530

    Update round_office.py

--- examples/round_office.py
+++ examples/round_office.py
@@ -18,7 +18,7 @@ async def run():
     """ Does Offboard control using position NED coordinates. """
 
     drone = System()
-    await drone.connect(system_address="serial:///dev/ttyHS1:2000000")
+    await drone.connect(system_address="udp://:14551")
 
     print("Waiting for drone to connect...")
     async for state in drone.core.connection_state():
